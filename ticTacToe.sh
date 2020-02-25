c457558ad650d638581efda167449eeb5d33f9f8
vkshay3400 <akshayp2208@gmail.com>
2020-02-25 19:11:46 +0530

    Added: Start TicTacToe

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,3 +1,16 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo "*************************TicTacToe*************************"
+
+# DECLARING BOARD
+declare -A board
+
+# DISPLAY BOARD
+function getBoard(){
+echo " | " ${board[1]} " | " ${board[2]} " | " ${board[3]} " | "
+echo "------------------"
+echo " | " ${board[4]} " | " ${board[5]} " | " ${board[6]} " | "
+echo "------------------"
+echo " | " ${board[7]} " | " ${board[8]} " | " ${board[9]} " | "
+echo "------------------"
+}
