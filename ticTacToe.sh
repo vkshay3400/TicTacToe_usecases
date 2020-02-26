607839e95433084eddc46f2f72e443d89b35face
vkshay3400 <akshayp2208@gmail.com>
2020-02-26 18:46:04 +0530

    Added: Display board and valid

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -2,50 +2,74 @@
 
 echo "*************************TicTacToe*************************"
 
-# CONSTANT
+# CONSTANTS
 SIGN=0
+BOARD_KEYS=9
 
-# RESETTING THE BOARD
-declare -A board
+# STORE IN ARRAY
+declare -a board
 
-# CONSTANT
-SIGN=0
+# INITIAL BOARD VALUES
+for (( index1=1; index1<=BOARD_KEYS; index1++ ))
+do
+	board[index1]="0"
+done
 
-# DISPLAY BOARD
+# FUNCTION TO GET BOARD
 function getBoard(){
+echo "--------------------"
 echo " | " ${board[1]} " | " ${board[2]} " | " ${board[3]} " | "
-echo "---------------------"
+echo "--------------------"
 echo " | " ${board[4]} " | " ${board[5]} " | " ${board[6]} " | "
-echo "---------------------"
+echo "--------------------"
 echo " | " ${board[7]} " | " ${board[8]} " | " ${board[9]} " | "
-echo "---------------------"
+echo "--------------------"
 }
+getBoard
 
-# TOSS
-function getToss(){
-   randomToss=$((RANDOM%2))
-   if [ $randomToss -eq 1 ]
-   then
-		toss=1
-      echo "Player1 win toss"
-   else
-      toss=0
-      echo "Player2 win toss"
-   fi
-}
-
-# FUNCTION FOR ASSIGNING THE PLAYER
-function playerAssign(){
-getToss $toss
-if [ $toss -eq $SIGN ]
+# FUNCTION FOR TOSS
+function toss(){
+randomToss=$((RANDOM%2))
+echo "RandToss :"$randomToss
+if [ $randomToss -eq $SIGN ]
 then
-	Player1=0
-	Player2=X
+	echo "Player 1 wins toss"
+	player1Choice=X
+	player2Choice=O
 else
-	Player1=X
-	Player2=0
+	echo "Player2 wins toss"
+	player1Choice=O
+   player2Choice=X
 fi
 }
 
-# FUNCTION CALL
-playerAssign
+# FUNCTION TO CHECK EMPTY PLACE
+function isEmpty(){
+	local position=$1
+	local playerChoice=$2
+	if [[ "${board[$position]}" == "0" ]]
+	then
+		board[$position]=$playerChoice
+		echo $((board[$position]))
+	else
+		echo "Enter again"
+		#board[$position1]=$player1Choice
+	fi
+}
+
+# FUNCTION FOR BOARD
+function board(){
+toss
+for (( index2=1; index2<=5; index2++))
+do
+	read -p "Player1 enter a position:" position1
+	isEmpty $position1 $player1Choice
+	getBoard
+	read -p "Player2 enter a position:" position2
+	isEmpty $position2 $player2Choice
+	getBoard
+done
+}
+
+# CALL FUNCTION
+board
