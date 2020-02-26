a427a9057f874b4d21d214f069f78e17c1569ccf
vkshay3400 <akshayp2208@gmail.com>
2020-02-26 18:39:28 +0530

    Added: Toss

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -8,24 +8,44 @@ SIGN=0
 # RESETTING THE BOARD
 declare -A board
 
+# CONSTANT
+SIGN=0
+
 # DISPLAY BOARD
 function getBoard(){
 echo " | " ${board[1]} " | " ${board[2]} " | " ${board[3]} " | "
-echo "------------------"
+echo "---------------------"
 echo " | " ${board[4]} " | " ${board[5]} " | " ${board[6]} " | "
-echo "------------------"
+echo "---------------------"
 echo " | " ${board[7]} " | " ${board[8]} " | " ${board[9]} " | "
-echo "------------------"
+echo "---------------------"
 }
 
-# FUNCTION TO ASSIGN PLAYER
+# TOSS
+function getToss(){
+   randomToss=$((RANDOM%2))
+   if [ $randomToss -eq 1 ]
+   then
+		toss=1
+      echo "Player1 win toss"
+   else
+      toss=0
+      echo "Player2 win toss"
+   fi
+}
+
+# FUNCTION FOR ASSIGNING THE PLAYER
 function playerAssign(){
-randomToss1=$((RANDOM%2))
-if [ $randomToss1 -eq $SIGN ]
+getToss $toss
+if [ $toss -eq $SIGN ]
 then
-	echo Player=X
+	Player1=0
+	Player2=X
 else
-	echo Player=0
+	Player1=X
+	Player2=0
 fi
 }
+
+# FUNCTION CALL
 playerAssign
