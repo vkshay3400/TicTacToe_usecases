4a65ed49b5e747b97183cb6e9fb55012ce9c50c9
vkshay3400 <akshayp2208@gmail.com>
2020-03-02 16:53:31 +0530

    Added: Play turn and winner

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,75 +1,202 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "*************************TicTacToe*************************"
 
 # CONSTANTS
-SIGN=0
-BOARD_KEYS=9
+BOARD_POSITION=9
+ROWS=3
+COLUMNS=3
+HEAD=1
+TAIL=0
+
+# VARIABLES
+playerTurn=false
+winner=false
+turn=0
+player1=0
+player2=0
+nonEmptyCount=1
 
 # STORE IN ARRAY
 declare -a board
 
-# INITIAL BOARD VALUES
-for (( index1=1; index1<=BOARD_KEYS; index1++ ))
-do
-	board[index1]="0"
-done
-
-# FUNCTION TO GET BOARD
-function getBoard(){
-echo "--------------------"
-echo " | " ${board[1]} " | " ${board[2]} " | " ${board[3]} " | "
-echo "--------------------"
-echo " | " ${board[4]} " | " ${board[5]} " | " ${board[6]} " | "
-echo "--------------------"
-echo " | " ${board[7]} " | " ${board[8]} " | " ${board[9]} " | "
-echo "--------------------"
-}
-getBoard
-
-# FUNCTION FOR TOSS
-function toss(){
-randomToss=$((RANDOM%2))
-echo "RandToss :"$randomToss
-if [ $randomToss -eq $SIGN ]
-then
-	echo "Player 1 wins toss"
-	player1Choice=X
-	player2Choice=O
-else
-	echo "Player2 wins toss"
-	player1Choice=O
-   player2Choice=X
-fi
+# FUNCTION FOR BOARD INITAILISE
+function boardInitialize(){
+	for (( index=1 ; index<=$BOARD_POSITION; index++ ))
+	do
+		board[$index]=0
+	done
 }
 
-# FUNCTION TO CHECK EMPTY PLACE
-function isEmpty(){
-	local position=$1
-	local playerChoice=$2
-	if [[ "${board[$position]}" == "0" ]]
+# DISPLAY BOARD
+function  displayBoard(){
+	echo " ------------- "
+	echo " | "${board[1]}" | "${board[2]}" | "${board[3]}" | "
+	echo " ------------- "
+	echo " | "${board[4]}" | "${board[5]}" | "${board[6]}" | "
+	echo " ------------- "
+	echo " | "${board[7]}" | "${board[8]}" | "${board[9]}" | "
+	echo " ------------- "
+}
+
+# HORIZONTAL CHECK
+function checkHorizontal(){
+	local position=1
+	counter=1
+	while [ $counter -le $ROWS ]
+	do
+		if [[ ${board[$position]} == ${board[$position+1]} ]] && [[ ${board[$position+1]} == ${board[$position+2]} ]] && [[ ${board[$position+2]} == $1 ]]
+		then
+			displayBoard
+			echo "$1 wins"
+			winner=true
+			break
+		else
+			position=$(( $position+$ROWS ))
+		fi
+	counter=$(( $counter+1 ))
+	done
+}
+
+# VERTICAL CHECK
+function checkVertical(){
+	position=1
+	counter=1
+	while [ $counter -le $ROWS ]
+	do
+		if [[ ${board[$position]} == ${board[$position+3]} ]] && [[ ${board[$position+3]}  ==  ${board[$position+6]} ]] && [[ ${board[$position+6]} == $1 ]]
+		then
+			displayBoard
+			echo "$1 Wins"
+			winner=true
+			break
+		else
+			position=$(( $position+1 ))
+		fi
+	counter=$(( $counter+1 ))
+	done
+}
+
+# DIAGONAL CHECK
+function checkDiagonal(){
+	position=1
+	counter=1
+	while [ $counter -le 2 ]
+	do
+		if [[ ${board[$position]} == ${board[$position+4]} ]] && [[ ${board[$position+4]}  ==  ${board[$position+8]} ]] && [[ ${board[$position+8]} == $1 ]]
+		then
+			displayBoard
+			echo " $1 wins "
+			winner=true
+			break
+		elif [[ ${board[$position+2]} == ${board[$position+4]} ]] && [[  ${board[$position+4]}  ==  ${board[$position+6]} ]] && [[ ${board[$position+6]} == $1 ]]
+		then
+			displayBoard
+			echo " $1 wins "
+			winner=true
+			break
+		fi
+		counter=$(($counter+1))
+	done
+}
+
+# CHECK TIE CASE
+function checkTie(){
+	while [[ ${board[$nonEmptyCount]} -ne 0 ]]
+	do
+		if [ $nonEmptyCount -eq $BOARD_POSITION ]
+		then
+			displayBoard
+			echo "Game is tie"
+			winner=true
+			break
+		else
+			nonEmptyCount=$(($nonEmptyCount+1))
+		fi
+	done
+}
+
+# USER INPUT1
+function userInput1() {
+	row=1
+	column=3
+	winMovePlayer1=false
+	read -p "Enter player1 in between 1 to 9: " POSITION
+	if [ $winMovePlayer1 == false ]
 	then
-		board[$position]=$playerChoice
-		echo $((board[$position]))
+		if [ ${board[$POSITION]} -eq $TAIL ]
+		then
+			echo "Player turn"
+			board[$POSITION]=$player1
+			displayBoard
+			turn=$(( $turn + 1 ))
+		else
+			echo "Invalid input"
+         userInput1
+		fi
+	fi
+	playerTurn=false
+}
+
+# USER INPUT2
+function userInput2() {
+	row=1
+	column=3
+	winMovePlayer2=false
+	read -p "Enter player2 in between 1 to 9: " POSITION
+	if [ $winMovePlayer2 == false ]
+	then
+		if [ ${board[$POSITION]} -eq $TAIL ]
+		then
+			echo "Player turn"
+			board[$POSITION]=$player2
+			displayBoard
+			turn=$(( $turn + 1 ))
+		else
+			echo "Invalid input"
+         userInput2
+		fi
+	fi
+	playerTurn=true
+}
+
+# FUNCTION FOR TIC TAC TOE
+function ticTacToe(){
+	checkingToss=$((RANDOM%2))
+	if [[ $checkingToss -eq $HEAD ]]
+	then
+		playerTurn=true
+		player1='X'
+		player2='O'
+		echo "Player1 will play first"
+		displayBoard
 	else
-		echo "Enter again"
-		#board[$position1]=$player1Choice
+		player2='X'
+		player1='O'
+		echo "Player2 will play first"
 	fi
 }
 
-# FUNCTION FOR BOARD
-function board(){
-toss
-for (( index2=1; index2<=5; index2++))
-do
-	read -p "Player1 enter a position:" position1
-	isEmpty $position1 $player1Choice
-	getBoard
-	read -p "Player2 enter a position:" position2
-	isEmpty $position2 $player2Choice
-	getBoard
-done
-}
+# FUNCTION CALL
+boardInitialize
+ticTacToe
 
-# CALL FUNCTION
-board
+# CHECK WHO WINS
+while [ $winner == false ]
+do
+	displayBoard
+	if [ $playerTurn == true ]
+	then
+		userInput1 $player1
+		checkHorizontal $player1
+		checkVertical $player1
+		checkDiagonal $player1
+		checkTie $player1
+	else
+		userInput2 $player2
+		checkHorizontal $player2
+		checkVertical $player2
+		checkDiagonal $player2
+		checkTie $player2
+	fi
+done
